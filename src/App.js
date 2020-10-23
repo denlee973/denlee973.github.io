4843aad7ff00b541ec631925c55c5bd8564883fd
Denny <denlee973@gmail.com>
2020-10-22 23:20:01 -0400

    css styling of name to be responsive to width

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div/>
-      <h1>denny lee.</h1>
+      <div />
+      <h1 className="name">denny lee.</h1>
     </div>
   );
 }
