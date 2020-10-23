eb91e5950b957231ab41142d5a70583773daa188
Denny <denlee973@gmail.com>
2020-10-23 00:06:03 -0400

    styling and add wip subtitle

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div />
       <h1 className="name">denny lee.</h1>
+      <h3 className="subtitle">work in progress</h3>
     </div>
   );
 }
