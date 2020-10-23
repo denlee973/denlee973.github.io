dff53f08e1516ffbc116a32a4db0fb252ccc4601
Denny <denlee973@gmail.com>
2020-10-22 22:23:40 -0400

    Updates

---
+++ precache-manifest.95f8185c90e58f11d4ea82e1e9137891.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70ec618590d295897e5abcef81ed1c4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e44c05b2fae9127204ec",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "6ce366e434fafe48cd75",
+    "url": "/static/js/2.0ef9824a.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.0ef9824a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e44c05b2fae9127204ec",
+    "url": "/static/js/main.abec3818.chunk.js"
+  },
+  {
+    "revision": "3e8dbb0797ae3048eca3",
+    "url": "/static/js/runtime-main.1ad1605b.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
